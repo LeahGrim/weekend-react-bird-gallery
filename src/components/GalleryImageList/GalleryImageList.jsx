267398075ccad2d7278273a97d28a386c27602e5
Leah Grim <grimchelate@gmail.com>
2022-01-15 15:24:30 -0600

    pictures uploaded with some styling

---
+++ src/components/GalleryImageList/GalleryImageList.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './GalleryImageList.css';
+function GalleryImageList(){
+
+    return(
+        <>
+        <div className= "ImageGallery">
+                <img src= "images/GGO.jpg"/>
+       
+        <br  />
+                <img src= "images/GBH.jpg"/>
+          
+        <br  />
+                <img src= "images/Sunbittern.jpg"/>
+           
+        <br  />
+            <img src= "images/AFO.jpeg"/>
+
+        <br  />
+            <img src= "images/barnowl.jpeg" />
+       
+        </div>
+        </>
+    )
+}
+
+export default GalleryImageList
