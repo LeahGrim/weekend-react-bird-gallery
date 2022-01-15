33d3b456ade2c8149ae2b23d4d5b425d50ff243d
Leah Grim <grimchelate@gmail.com>
2022-01-15 15:39:33 -0600

    wasnt working on branch

--- src/components/GalleryImageList/GalleryImageList.jsx
+++ src/components/GalleryImageList/GalleryImageList.jsx
@@ -5,20 +5,7 @@ function GalleryImageList(){
     return(
         <>
         <div className= "ImageGallery">
-                <img src= "images/GGO.jpg"/>
-       
-        <br  />
-                <img src= "images/GBH.jpg"/>
-          
-        <br  />
-                <img src= "images/Sunbittern.jpg"/>
-           
-        <br  />
-            <img src= "images/AFO.jpeg"/>
-
-        <br  />
-            <img src= "images/barnowl.jpeg" />
-       
+                
         </div>
         </>
     )
