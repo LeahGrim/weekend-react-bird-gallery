0271d73a06f0c0d1996da9115b2c07f9f1c5e01b
Leah Grim <grimchelate@gmail.com>
2022-01-15 14:29:05 -0600

    adorable header completed

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+function Header(){
+    return(
+        <header className="banner-header">
+            <h1> Birds Engrained in My Mind Forever </h1>
+            <h3> "in all poor foolish things that live a day, eternal beauty wandering on her way" -Yeats </h3>
+
+        </header>
+    )
+
+}
+export default Header;
